98f9ad00d8f5e7dbc1c75ad5f7aae97c6694ad04
Pasha Emshanov <paemshanov@edu.hse.ru>
2021-07-05 23:54:25 +0300

    Practice 1 Emshanov Pavel

--- practice/1_opencv/cat_passport.py
+++ practice/1_opencv/cat_passport.py
@@ -1,26 +1,45 @@
 import argparse
 import cv2
-
+import sys
+import numpy as np
 
 def make_cat_passport_image(input_image_path, haar_model_path):
 
     # Read image
+    image = cv2.imread(input_image_path)
 
     # Convert image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Normalize image intensity
+    gray = cv2.equalizeHist(gray)
+    # gray = cv2.normalizeHist(gray)
 
     # Resize image
+    resized = cv2.resize(gray, (640, 480), interpolation=cv2.INTER_AREA)
 
     # Detect cat faces using Haar Cascade
+    detector = cv2.CascadeClassifier(haar_model_path)
+    rects = detector.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(75, 75))
 
     # Draw bounding box
+    for (i, (x, y, w, h)) in enumerate(rects):
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.putText(image, "Cat #{}".format(i + 1), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
 
     # Display result image
+    cv2.imshow("Full result", image)
 
     # Crop image
+    x, y, w, h = rects[0]
+    image = image[y:y+h, x:x+w]
 
     # Save result image to file
+    cv2.imwrite('out.jpg', image)
+
+    cv2.imshow("Created image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
     return
 
