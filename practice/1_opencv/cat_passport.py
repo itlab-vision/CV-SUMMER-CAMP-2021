53024226f1fda686aa2ba97f247369e76903198f
Dmitry Semenov <lonelyteapot@gmail.com>
2021-07-06 14:25:31 +0300

    Implement cat passport generation

--- practice/1_opencv/cat_passport.py
+++ practice/1_opencv/cat_passport.py
@@ -1,6 +1,7 @@
 import argparse
 import cv2
 import sys
+from random import randint, choice
 
 
 def detect_cat_in_image(input_image_path, haar_model_path):
@@ -44,6 +45,53 @@ def detect_cat_in_image(input_image_path, haar_model_path):
     # cv2.imwrite('out.jpg', image)
 
 
+def make_cat_passport(cat_image, passport_image_path, output_path):
+
+    # Read passport image
+    passport_image = cv2.imread(passport_image_path)
+
+    x1, y1 = 31, 48
+    x2, y2 = 200+1, 186+1
+
+    # Resize cat image to fit
+    cat_image = cv2.resize(cat_image, (x2-x1, y2-y1), interpolation=cv2.INTER_AREA)
+
+    # Put cat image on passport
+    passport_image[y1:y2, x1:x2] = cat_image
+
+    # Put text
+    cv2.putText(
+        passport_image, choice(('Max', 'Chloe', 'Bella', 'Oliver', 'Smokey', 'Lucy', 'Charlie')),
+        (100, 218), cv2.FONT_HERSHEY_COMPLEX, 0.64, (10, 10, 10), 1)
+    cv2.putText(
+        passport_image, f'{randint(1, 30)}:{randint(1,12):02}:2020',
+        (122, 272), cv2.FONT_HERSHEY_SIMPLEX, 0.33, (10, 10, 10), 1)
+    cv2.putText(
+        passport_image, ''.join(choice('0123456789') for _ in range(14)),
+        (275, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.49, (10, 10, 10), 1)
+    cv2.putText(
+        passport_image, f'{randint(1, 30)}:{randint(1,6):02}:2021',
+        (275, 130), cv2.FONT_HERSHEY_SIMPLEX, 0.49, (10, 10, 10), 1)
+    cv2.putText(
+        passport_image, 'Shoulder Blades',
+        (275, 170), cv2.FONT_HERSHEY_SIMPLEX, 0.49, (10, 10, 10), 1)
+    cv2.putText(
+        passport_image, ''.join(choice('0123456789') for _ in range(14)), 
+        (275, 210), cv2.FONT_HERSHEY_SIMPLEX, 0.49, (10, 10, 10), 1)
+    cv2.putText(
+        passport_image, f'{randint(1, 30)}:{randint(1,6):02}:2021',
+        (275, 250), cv2.FONT_HERSHEY_SIMPLEX, 0.49, (10, 10, 10), 1)
+
+    # Display passport
+    cv2.imshow("Cat Passport", passport_image)
+
+    # Save passport to file
+    if output_path:
+        cv2.imwrite(output_path, passport_image)
+        print(f'Image saved to {output_path}')
+
+
+
 def build_argparser():
     parser = argparse.ArgumentParser(
         description='Speech denoising demo', add_help=False)
@@ -54,6 +102,10 @@ def build_argparser():
                       help='Required. Path to .XML file with pre-trained model.')
     args.add_argument('-i', '--input', type=str, required=True,
                       help='Required. Path to input image')
+    args.add_argument('-p', '--passport', type=str, required=True,
+                      help='Required. Path to passport image')
+    args.add_argument('-o', '--output', type=str, required=False,
+                      help='Passport output path')
     return parser
 
 
@@ -64,6 +116,7 @@ def main():
     if cat_image is None:
         print('No cat was detected in the image :(')
         return 0
+    make_cat_passport(cat_image, args.passport, args.output)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
