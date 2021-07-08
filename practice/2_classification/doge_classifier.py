22c2a8919665f8dd645372bc1805fd6d42e28007
Arapov Denis <ArapovZWERR@yandex.ru>
2021-07-08 18:10:04 +0300

    Multiple image proccesing added

--- practice/2_classification/doge_classifier.py
+++ practice/2_classification/doge_classifier.py
@@ -18,33 +18,57 @@ from openvino.inference_engine import IENetwork, IECore
 class InferenceEngineClassifier:
     def __init__(self, configPath=None, weightsPath=None,
             device='CPU', extension=None, classesPath=None):
-        
-        # Add code for Inference Engine initialization
-        
-        # Add code for model loading
 
+        # Add code for Inference Engine initialization
+        self.ie = IECore()
+        # Add code for model loading
+        self.net = self.ie.read_network(model=configPath)
+        self.exec_net = self.ie.load_network(network=self.net, device_name=device)
         # Add code for classes names loading
-        
+        if classesPath is None:
+            # Generate numbered class names
+            self.classes = [(i, '') for i in range(1000)]
+        else:
+            # Load class names from file
+            with open(classesPath, 'r') as fp:
+                self.classes = [tuple(line.rstrip().split(maxsplit=1)) for line in fp]
+
         return
 
-    def get_top(self, prob, topN=1):
-        result = []
-        
-        # Add code for getting top predictions
-        
-        return result
+    def get_top(self, images_probs, topN=1):
+        # Assoisiate probabilities with class names
+        result = [dict() for i in range(len(images_probs))]
+        for i, image_probs in enumerate(images_probs):
+            for j, prob in enumerate(image_probs):
+                for k in range(len(prob)):
+                    result[i][self.classes[k][0]] = prob[k][0][0]
+
+        # Return top entries based on highest probabilities
+        return [sorted(result[i].items(), key=lambda i: i[1], reverse=True)[:topN] for i in range(len(result))]
 
     def _prepare_image(self, image, h, w):
-    
+
         # Add code for image preprocessing
-        
+        image = cv2.resize(image, (w, h))
+        image = image.transpose((2, 0, 1))
         return image
 
-    def classify(self, image):
-        probabilities = None
-        
-        # Add code for image classification using Inference Engine
-        
+    def classify(self, images):
+        # Get data about input and output from neural network
+        input_blob = next(iter(self.net.input_info))
+        output_blob = next(iter(self.net.outputs))
+
+        # Get required input shape for input
+        n, c, h, w = self.net.inputs[input_blob].shape
+
+        images_new = [np.ndarray(shape=(n, c, h, w)) for i in range(len(images))]
+        for i, image in enumerate(images):
+            images_new[i][0] = self._prepare_image(image, h, w)
+
+        # Classify the images and get result tensors
+        output = [self.exec_net.infer(inputs={input_blob: images_new[i]}) for i in range(len(images))]
+        probabilities = [output[i][output_blob] for i in range(len(images))]
+
         return probabilities
 
 
@@ -68,6 +92,10 @@ def build_argparser():
     return parser
 
 
+def get_images(image_dir_path):
+    return os.listdir(os.path.join(*image_dir_path.split("/")))
+
+
 def main():
     log.basicConfig(format="[ %(levelname)s ] %(message)s",
         level=log.INFO, stream=sys.stdout)
@@ -76,14 +104,19 @@ def main():
     log.info("Start IE classification sample")
 
     # Create InferenceEngineClassifier object
-    
-    # Read image
-        
-    # Classify image
-    
-    # Get top 5 predictions
-    
-    # print result
+    ie_classifier = InferenceEngineClassifier(configPath=args.model,
+                                              weightsPath=args.weights, device=args.device,
+                                              extension=args.cpu_extension, classesPath=args.classes)
+    image_names = get_images(args.input)
+    # Read images
+    images = [cv2.imread(os.path.join(args.input, image_name)) for image_name in image_names]
+    # Classify images
+    probs = ie_classifier.classify(images)
+    # Get top 5 predictions for each
+    predictions = ie_classifier.get_top(probs, 5)
+    # Show
+    for i, image_name in enumerate(image_names):
+        log.info(f"Predictions for {image_name}: " + str(predictions[i]))
 
     return
 
