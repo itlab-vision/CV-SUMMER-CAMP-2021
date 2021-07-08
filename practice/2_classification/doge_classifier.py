3abe93a444d1beba584a3419d9f2c9347964e053
Dmitry Semenov <lonelyteapot@gmail.com>
2021-07-08 15:33:05 +0300

    Restyle argparser

--- practice/2_classification/doge_classifier.py
+++ practice/2_classification/doge_classifier.py
@@ -69,21 +69,32 @@ class InferenceEngineClassifier:
 
 def build_argparser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model', help='Path to an .xml \
-        file with a trained model.', required=True, type=str)
-    parser.add_argument('-w', '--weights', help='Path to an .bin file \
-        with a trained weights.', required=True, type=str)
-    parser.add_argument('-i', '--input', help='Path to \
-        image file', required=True, type=str)
-    parser.add_argument('-l', '--cpu_extension', help='MKLDNN \
-        (CPU)-targeted custom layers.Absolute path to a shared library \
-        with the kernels implementation', type=str, default=None)
-    parser.add_argument('-d', '--device', help='Specify the target \
-        device to infer on; CPU, GPU, FPGA or MYRIAD is acceptable. \
-        Sample will look for a suitable plugin for device specified \
-        (CPU by default)', default='CPU', type=str)
-    parser.add_argument('-c', '--classes', help='File containing classes \
-        names', type=str, default=None)
+    parser.add_argument(
+        '-i', '--input', type=str, default='input',
+        help='Path to a folder with image files'
+    )
+    parser.add_argument(
+        '-m', '--model', required=True, type=str,
+        help='Path to an .xml file with a trained model'
+    )
+    parser.add_argument(
+        '-w', '--weights', required=True, type=str,
+        help='Path to a .bin file with a trained weights'
+    )
+    parser.add_argument(
+        '-c', '--classes', type=str, default=None,
+        help='Path to a file containing classes names'
+    )
+    parser.add_argument(
+        '-l', '--cpu_extension', type=str, default=None,
+        help='MKLDNN (CPU)-targeted custom layers. \
+        Absolute path to a shared library with the kernels implementation'
+    )
+    parser.add_argument(
+        '-d', '--device', type=str, default='CPU',
+        help='The target device to infer on; CPU, GPU, FPGA or MYRIAD is acceptable. \
+        Sample will look for a suitable plugin for device specified (CPU by default)'
+    )
     return parser
 
 
