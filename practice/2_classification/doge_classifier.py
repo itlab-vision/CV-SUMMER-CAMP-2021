99c791d6918920c6b47d9dcaab84eec320d60710
Makarov-AA <43129398+Makarov-AA@users.noreply.github.com>
2021-07-09 09:01:46 +0300

    Some attempts for additional task

--- practice/2_classification/doge_classifier.py
+++ practice/2_classification/doge_classifier.py
@@ -16,6 +16,8 @@ from openvino.inference_engine import IENetwork, IECore
 
 
 class InferenceEngineClassifier:
+    _device = 'CPU'
+
     def __init__(self, configPath=None, weightsPath=None,
             device='CPU', extension=None, classesPath=None):
         
@@ -23,39 +25,69 @@ class InferenceEngineClassifier:
         self.ie = IECore()
         # Add code for model loading
         self.net = self.ie.read_network(model=configPath)
-        self.exec_net = self.ie.load_network(network=self.net, device_name=device)
-        # Add code for classes names loading
+        
+        self._device = device
         
         return
 
     def get_top(self, prob, topN=1):
-        result = []
+        results = []
         
         # Add code for getting top predictions
-        result = np.squeeze(prob)
-        top_classes = np.argsort(result)[-topN :][::-1]
-        top_probs = [result[x] for x in top_classes]
+        for result in prob:
+            result = np.squeeze(result)
+            top_classes = np.argsort(result)[-topN :][::-1]
+            top_probs = [result[x] for x in top_classes]
+            results.append((top_classes, top_probs))
         
-        return top_classes, top_probs
+        return results
 
-    def _prepare_image(self, image, h, w):
-    
+    def prepare_input(self, input):
+
+        # Read images
+        imgs_path = []
+        is_correct = True
+        input = input.split(',')
+        if os.path.exists(input[0]):
+            if os.path.isdir(input[0]):
+                path = os.path.abspath(input[0])
+                imgs_path = [os.path.join(path, file) for file in os.listdir(path)]
+            elif os.path.isfile(input[0]):
+                for image in input:
+                    if not os.path.isfile(image):
+                        raise ValueError('Incorrect input!')
+                    imgs_path.append(os.path.abspath(image))
+            else:
+                raise ValueError('Incorrect input!')
+        
         # Add code for image preprocessing
-        image = cv2.resize(image, (w, h))
-        image = image.transpose((2, 0, 1))
-        return image
+        input_blob = next(iter(self.net.inputs))
+        n, c, h, w = self.net.inputs[input_blob].shape
+        images = []
+        for path in imgs_path:
+            image = cv2.imread(path)
+            image = cv2.resize(image, (h, w))
+            images.append(image)
+        images = np.array(images)
+        images = images.transpose((0, 3, 1, 2))
+        return images
 
-    def classify(self, image):
+    def classify(self, images):
         
         # Add code for image classification using Inference Engine
-        input_blob = next(iter(self.net.inputs))
         out_blob = next(iter(self.net.outputs))
+        input_blob = next(iter(self.net.inputs))
 
-        n, c, h, w = self.net.inputs[input_blob].shape
+        new_shapes = {}
+        for layer in self.net.inputs:
+            shape = self.net.inputs[layer].shape
+            shape[0] = len(images)
+            new_shapes.update({layer: shape})
+        self.net.reshape(new_shapes)
 
-        image = self._prepare_image(image, h, w)
+        self.exec_net = self.ie.load_network(network=self.net, device_name=self._device)
 
-        output = self.exec_net.infer(inputs = {input_blob: image})
+        output = self.exec_net.infer(inputs = {input_blob: images})
 
         output = output[out_blob]
         return output
@@ -90,25 +122,26 @@ def main():
 
     # Create InferenceEngineClassifier object
     ie_classifier = InferenceEngineClassifier(configPath=args.model, 
-        weightsPath=args.weights, device=args.device, 
+        weightsPath=args.weights, device='GPU', 
         extension=args.cpu_extension, classesPath=args.classes)
 
     # Read image
-    img = cv2.imread(args.input)
+    images = ie_classifier.prepare_input(args.input)
 
     # Classify image
-    prob = ie_classifier.classify(img)
+    prob = ie_classifier.classify(images)
     
     # Get top 5 predictions
-    top_classes, top_probs = ie_classifier.get_top(prob, 5)
+    results = ie_classifier.get_top(prob, 5)
     
     # print result
     with open(args.classes, 'r') as f:
         classes = [line.strip('\n') for line in f]
-        log.info("Predictions:\n" + 
-            '\n'.join([classes[idx][10 :] + ' ' + str(top_probs[i]) for i, idx in enumerate(top_classes)]))
-    
-    
+        for i, result in enumerate(results):
+            top_classes = result[0]
+            top_probs = result[1]
+            log.info("Predictions for image {}:\n".format(i) + 
+                '\n'.join([classes[idx][10 :] + ' ' + str(top_probs[j]) for j, idx in enumerate(top_classes)]))
     
     return
 
