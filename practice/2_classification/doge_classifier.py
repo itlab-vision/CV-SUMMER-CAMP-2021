85b77e2c88684c4b8271882f00d5b4c533d010eb
Артем Савкин <58187114+OrangeRedeng@users.noreply.github.com>
2021-07-12 20:42:10 +0300

    Update doge_classifier.py

--- practice/2_classification/doge_classifier.py
+++ practice/2_classification/doge_classifier.py
@@ -20,16 +20,22 @@ class InferenceEngineClassifier:
             device='CPU', extension=None, classesPath=None):
         
         # Add code for Inference Engine initialization
-        
+        self.ie = IECore()
+
         # Add code for model loading
+        self.net = self.ie.read_network(model=configPath)
+        self.exec_net = self.ie.load_network(network=self.net, device_name=device)
 
         # Add code for classes names loading
-        
+        #with open(args.labels, 'r') as f:
+            #labels_map = [x.split(sep=' ', maxsplit=1)[-1].strip() for x in f]
         return
 
     def get_top(self, prob, topN=1):
         result = []
         
+        result = np.squeeze(prob)
+        result = np.argsort(result)[-topN:][::-1]
         # Add code for getting top predictions
         
         return result
@@ -37,15 +43,26 @@ class InferenceEngineClassifier:
     def _prepare_image(self, image, h, w):
     
         # Add code for image preprocessing
-        
+        image = cv2.resize(image, (w, h))
+        image = image.transpose((2, 0, 1))
         return image
 
     def classify(self, image):
         probabilities = None
         
         # Add code for image classification using Inference Engine
-        
-        return probabilities
+        input_blob = next(iter(self.net.inputs))
+        out_blob = next(iter(self.net.outputs))
+
+        n, c, h, w = self.net.inputs[input_blob].shape
+
+        image = self._prepare_image(image, h, w)
+
+        output = self.exec_net.infer(inputs = {input_blob: image})
+
+        output = output[out_blob]
+
+        return output
 
 
 def build_argparser():
@@ -76,14 +93,24 @@ def main():
     log.info("Start IE classification sample")
 
     # Create InferenceEngineClassifier object
-    
+    ie_classifier = InferenceEngineClassifier(
+        configPath=args.model,
+        weightsPath=args.weights, 
+        device=args.device,
+        extension='CPU', 
+        classesPath=args.classes)
+
     # Read image
+    img = cv2.imread(args.input)
         
     # Classify image
+    prob = ie_classifier.classify(img)
     
     # Get top 5 predictions
+    predictions = ie_classifier.get_top(prob, 5)
     
     # print result
+    log.info("Predictions: " + str(predictions))
 
     return
 
