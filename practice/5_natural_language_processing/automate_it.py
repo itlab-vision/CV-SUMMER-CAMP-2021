300cd47025853176289493ccdb2b835a328bf6a4
Oleg <olejka647@gmail.com>
2021-07-16 16:18:10 +0300

    practice5

--- practice/5_natural_language_processing/automate_it.py
+++ practice/5_natural_language_processing/automate_it.py
@@ -4,8 +4,12 @@ import subprocess
 
 def parse_cmd_output(text):
     # Cut technical information from cmd output
-
-    return text
+    text_transform = text.split(b'[ INFO ]')
+    result = str()
+    for part_text in text_transform:
+        if part_text.startswith((b' Question', b' ---answer', b' Get')):
+            result += part_text.decode()
+    return result
 
 
 def build_argparser():
@@ -16,27 +20,46 @@ def build_argparser():
     return parser
 
 
+def generate_cmd(path_to_demo, path_to_model, sites, questions, res):
+    for site_tmp in sites:
+        for q in questions:
+            # Prepare text command line
+            cmd = f'python "{path_to_demo}" -v vocab.txt -m {path_to_model} --input="{site_tmp}" --questions "{q}"'
+            # Run subprocess using prepared command line
+            returned_output = subprocess.check_output(cmd)
+            # Process output
+            res += parse_cmd_output(returned_output)
+    return res
+
 def main():
     args = build_argparser().parse_args()
 
     # Prepare input parameters for script 
     path_to_demo = "C:/Program Files (x86)/Intel/openvino_2021.3.394/deployment_tools/open_model_zoo/demos/bert_question_answering_demo/python/bert_question_answering_demo.py"
     path_to_model = args.m #"bert-small-uncased-whole-word-masking-squad-0001/FP32/bert-small-uncased-whole-word-masking-squad-0001.xml"
-    question = "What operating system is required?"
-    site = "https://en.wikipedia.org/wiki/OpenVINO"
 
-    # Prepare text command line 
-    cmd = f'python "{path_to_demo}" -v vocab.txt -m {path_to_model} --input="{site}" --questions "{question}"'
+    cmd = str()
 
-    # Run subprocess using prepared command line
-    returned_output = subprocess.check_output(cmd)
+    questions = str()
+    
+    with open(args.q, 'r') as file_questions:
+        questions = file_questions.read().split('\n')
 
-    # Process output
-    answer = parse_cmd_output(returned_output)
+    sites = str()
+
+    with open(args.i, 'r') as file_sites:
+        sites = file_sites.read().split('\n')
+
+    res = str()
+
+    if len(sites) > 1:
+        res = generate_cmd(path_to_demo, path_to_model, sites, questions, res)
+    else:
+        res = generate_cmd(path_to_demo, path_to_model, sites, questions, res)
 
     # Write output to file
     with open('answer.txt', 'w') as the_file:
-        the_file.write(f'{answer}\n')
+        the_file.write(f'{res}\n')
 
 
 if __name__ == "__main__":
