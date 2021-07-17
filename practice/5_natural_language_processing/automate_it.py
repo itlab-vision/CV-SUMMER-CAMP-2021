a0e86ea714c526923cb1b5ed7fe4f233790f3272
Dmitry Semenov <lonelyteapot@gmail.com>
2021-07-17 18:27:17 +0300

    Implement reading multiple questions and sites

--- practice/5_natural_language_processing/automate_it.py
+++ practice/5_natural_language_processing/automate_it.py
@@ -36,22 +36,30 @@ def build_argparser():
 def main():
     args = build_argparser().parse_args()
 
-    # Prepare input parameters for script
-    path_to_demo = "C:/Program Files (x86)/Intel/openvino_2021/deployment_tools/open_model_zoo/demos/bert_question_answering_demo/python/bert_question_answering_demo.py"
-    path_to_model = args.m
-    question = "What operating system is required?"
-    site = "https://en.wikipedia.org/wiki/OpenVINO"
+    # Load input data
+    with open(args.q) as fp:
+        questions = [line.strip() for line in fp]
+    with open(args.i) as fp:
+        sites = [line.strip() for line in fp]
 
-    # Prepare text command line
-    cmd = f'./venv/Scripts/python "{path_to_demo}" -v vocab.txt -m {path_to_model} --input="{site}" --questions "{question}"'
+    answer = []
+    for site in sites:
+        # Prepare input parameters for script
+        path_to_demo = "C:/Program Files (x86)/Intel/openvino_2021/deployment_tools/open_model_zoo/demos/bert_question_answering_demo/python/bert_question_answering_demo.py"
+        path_to_model = args.m
+        questions_str = ' '.join(f'"{question}"' for question in questions)
 
-    # Run subprocess using prepared command line
-    returned_output = subprocess.check_output(cmd)
+        # Prepare text command line
+        cmd = f'./venv/Scripts/python "{path_to_demo}" -v vocab.txt -m {path_to_model} --input="{site}" --questions {questions_str}'
 
-    # Process output
-    answer = parse_cmd_output(returned_output)
+        # Run subprocess using prepared command line
+        returned_output = subprocess.check_output(cmd)
+
+        # Process output
+        answer.append(f'[{site}] {parse_cmd_output(returned_output)}')
 
     # Write output to file
+    answer = '\n'.join(answer)
     with open('answer.txt', 'w') as the_file:
         the_file.write(f'{answer}\n')
 
