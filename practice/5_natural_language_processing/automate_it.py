d8f8c46d44fc721dbfe221967592a5e4e8362f48
Смирнов Григорий <greg.smirnov2013@yandex.ru>
2021-07-18 22:38:49 +0300

    pratice 5 with dop tasks

--- practice/5_natural_language_processing/automate_it.py
+++ practice/5_natural_language_processing/automate_it.py
@@ -4,8 +4,12 @@ import subprocess
 
 def parse_cmd_output(text):
     # Cut technical information from cmd output
-
-    return text
+    info_strings = text.split(b'[ INFO ]')
+    result = str()
+    for s in info_strings:
+        if s.startswith(b' ---answer'):
+            result += s.decode()
+    return result
 
 
 def build_argparser():
@@ -20,23 +24,37 @@ def main():
     args = build_argparser().parse_args()
 
     # Prepare input parameters for script 
-    path_to_demo = "C:/Program Files (x86)/Intel/openvino_2021.3.394/deployment_tools/open_model_zoo/demos/bert_question_answering_demo/python/bert_question_answering_demo.py"
-    path_to_model = args.m #"bert-small-uncased-whole-word-masking-squad-0001/FP32/bert-small-uncased-whole-word-masking-squad-0001.xml"
-    question = "What operating system is required?"
-    site = "https://en.wikipedia.org/wiki/OpenVINO"
+    path_to_demo = "C:/Program Files (x86)/Intel/openvino_2021.4.582/deployment_tools/open_model_zoo/demos/bert_question_answering_demo/python/bert_question_answering_demo.py"
+    path_to_model = "intel/bert-small-uncased-whole-word-masking-squad-0001/FP32/bert-small-uncased-whole-word-masking-squad-0001.xml"
+    # question = "What operating system is required?"
+    # site = "https://en.wikipedia.org/wiki/OpenVINO"
 
-    # Prepare text command line 
-    cmd = f'python "{path_to_demo}" -v vocab.txt -m {path_to_model} --input="{site}" --questions "{question}"'
+    answers_log = list()
 
-    # Run subprocess using prepared command line
-    returned_output = subprocess.check_output(cmd)
+    with open(args.q, 'r') as file_q:
+        questions = file_q.read().split('\n')
 
-    # Process output
-    answer = parse_cmd_output(returned_output)
+    with open(args.i, 'r') as file_s:
+        sites = file_s.read().split('\n')
+
+    for site in sites:
+        answers_log.append("[ SITE ]:  " + site)
+        for question in questions:
+            # Prepare text command line
+            cmd = f'python "{path_to_demo}" -v vocab.txt -m {path_to_model} --input="{site}" --questions "{question}"'
+
+            # Run subprocess using prepared command line
+            returned_output = subprocess.check_output(cmd)
+
+            # Process output
+            answer = parse_cmd_output(returned_output)
+
+            answers_log.append(question + "\n" + answer)
 
     # Write output to file
-    with open('answer.txt', 'w') as the_file:
-        the_file.write(f'{answer}\n')
+    with open('answer.txt', 'w') as ans_file:
+        for answer in answers_log:
+            ans_file.write(f'{answer}\n')
 
 
 if __name__ == "__main__":
