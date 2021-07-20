f0ecbe48c33d4e79d78477168704506f55742282
VladislavLap <vladislav.laptev-cool@yandex.ru>
2021-07-20 20:41:23 +0300

    practice-5 main task

--- practice/5_natural_language_processing/automate_it.py
+++ practice/5_natural_language_processing/automate_it.py
@@ -1,11 +1,31 @@
+
 import argparse
 import subprocess
+#import sys
+#from pathlib import Path
 
+#print("PYTHONPATH:", os.environ.get('PYTHONPATH'))
+#print("PATH:", os.environ.get('PATH'))
+#sys.path.append('C:\Users\Владислав\Documents\Python Scripts\CV-SUMMER-CAMP-2021\practice\openvino-virtual-environments\openvinoenv\Lib\site-packages')
+#sys.path.append(str(Path(__file__).resolve().parents[1] / 'openvino-virtual-environments_2/openvinoenv/Lib/site-packages'))
 
 def parse_cmd_output(text):
     # Cut technical information from cmd output
-
-    return text
+    print(text)
+    
+    srch_str = b'---answer'
+    bias = len(srch_str)
+    res_str = b''
+    
+    cur_srch = text.find(srch_str)
+    while not cur_srch == -1 :
+        end_index = text.find(b'\r', cur_srch + bias )
+        res_str += text[cur_srch : end_index] + b' \n'
+        cur_srch = text.find(srch_str, end_index)  
+        print(res_str)
+    
+    print(str(res_str))
+    return res_str.decode('utf-8')
 
 
 def build_argparser():
@@ -21,12 +41,12 @@ def main():
 
     # Prepare input parameters for script 
     path_to_demo = "C:/Program Files (x86)/Intel/openvino_2021.3.394/deployment_tools/open_model_zoo/demos/bert_question_answering_demo/python/bert_question_answering_demo.py"
-    path_to_model = args.m #"bert-small-uncased-whole-word-masking-squad-0001/FP32/bert-small-uncased-whole-word-masking-squad-0001.xml"
-    question = "What operating system is required?"
-    site = "https://en.wikipedia.org/wiki/OpenVINO"
+    path_to_model = args.m # "C:\Users\Владислав\Documents\Python Scripts\CV-SUMMER-CAMP-2021\practice\5_natural_language_processing\intel\bert-small-uncased-whole-word-masking-squad-0001\FP16/bert-small-uncased-whole-word-masking-squad-0001/FP32/bert-small-uncased-whole-word-masking-squad-0001.xml"
+    question = args.q #"What operating system is required?"
+    site = args.i #"https://en.wikipedia.org/wiki/OpenVINO"
 
     # Prepare text command line 
-    cmd = f'python "{path_to_demo}" -v vocab.txt -m {path_to_model} --input="{site}" --questions "{question}"'
+    cmd = f'python "{path_to_demo}" -v "vocab.txt" -m "{path_to_model}" --input="{site}" --questions "{question}"'
 
     # Run subprocess using prepared command line
     returned_output = subprocess.check_output(cmd)
@@ -35,9 +55,9 @@ def main():
     answer = parse_cmd_output(returned_output)
 
     # Write output to file
-    with open('answer.txt', 'w') as the_file:
-        the_file.write(f'{answer}\n')
-
+    with open('answer.txt', 'a') as the_file:
+        the_file.write(f'{question}\n{answer}\n\n')
 
 if __name__ == "__main__":
+    
     main()
