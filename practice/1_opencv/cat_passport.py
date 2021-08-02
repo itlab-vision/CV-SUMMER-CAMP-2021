0757937940519476c292d6d500715ad8a609c093
a0x5f <a0x5f@ya.ru>
2021-08-02 21:12:33 +0300

    Practice 1
    Done

--- practice/1_opencv/cat_passport.py
+++ practice/1_opencv/cat_passport.py
@@ -1,26 +1,44 @@
 import argparse
+import sys
 import cv2
 
 
 def make_cat_passport_image(input_image_path, haar_model_path):
 
     # Read image
+    image = cv2.imread(input_image_path)
 
     # Convert image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Normalize image intensity
+    gray = cv2.equalizeHist(gray)
 
     # Resize image
+    #resized = cv2.resize(gray, (640, 480), interpolation = cv2.INTER_AREA)
 
     # Detect cat faces using Haar Cascade
-
+    detector = cv2.CascadeClassifier(haar_model_path)
+    rects = detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(75, 75))
+    
     # Draw bounding box
-
+    for (i, (x, y, w, h)) in enumerate(rects): 
+        thickness = 2
+        color = (198, 252, 3) # BGR
+        cv2.rectangle(image, (x - thickness, y - thickness), (x + w + thickness, y + h + thickness), color, thickness)
+        cv2.putText(image, "Cat #{}".format(i + 1), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.55, color, 2)
+    
     # Display result image
+    cv2.imshow("frontalcatface", image) 
+    cv2.waitKey(0) 
+    cv2.destroyAllWindows()
 
     # Crop image
+    x, y, w, h = rects[0] 
+    image = image[y:y+h, x:x+w]
 
     # Save result image to file
+    cv2.imwrite('out.jpg', image)
 
     return
 
